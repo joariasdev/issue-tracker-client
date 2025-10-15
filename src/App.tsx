6113ca23d00eb6db41a713682710f667f63769f1
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-10-15 19:46:38 -0400

    feat: display issues list on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+const dbData = [
+  { title: "Become a Professional Software Developer" },
+  { title: "Get Physically Fit" },
+  { title: "Earn 100K per month" },
+  { title: "Purchase a House" },
+  { title: "Start a Business" },
+];
+
 export default function App() {
+  const issues = dbData.map((issue) => <p>{issue.title}</p>);
+
   return (
     <>
       <h1>Issue Tracker</h1>
+      <div>{issues}</div>
     </>
   );
 }

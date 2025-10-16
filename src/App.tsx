93fde094f4510f5e3eb7ec92d4d22266c3d63f5f
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-10-15 20:43:14 -0400

    feat: add form to add new issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
 const dbData = [
   { title: "Become a Professional Software Developer" },
   { title: "Get Physically Fit" },
@@ -7,12 +9,31 @@ const dbData = [
 ];
 
 export default function App() {
-  const issues = dbData.map((issue) => <p>{issue.title}</p>);
+  const [issues, setIssues] = useState(dbData);
+  const [inputValue, setInputValue] = useState("");
+
+  const issueList = issues.map((issue) => <p>{issue.title}</p>);
+
+  const addIssue = (e: FormEvent) => {
+    e.preventDefault();
+    const newIssue = { title: inputValue };
+    dbData.push(newIssue);
+    setIssues(dbData);
+    setInputValue("");
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
 
   return (
     <>
       <h1>Issue Tracker</h1>
-      <div>{issues}</div>
+      <div>{issueList}</div>
+      <form onSubmit={addIssue}>
+        <input type="text" onChange={handleChange} value={inputValue} />
+        <input type="submit" value="Add" />
+      </form>
     </>
   );
 }
